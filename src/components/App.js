7fa2d6e136eaf8ed530921e308d5bfa4f54c9fe6
OmarHassin <boky555555@gmail.com>
2020-06-20 22:33:22 +0200

    Project Structure2

--- src/components/App.js
+++ src/components/App.js
@@ -1,77 +1,8 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
 import "../style/App.css";
 
-import { add_Reminder, remove_Reminder } from "../actions";
-
-class App extends Component {
-  state = {
-    text: "",
-    date: new Date(),
-  };
-  render_Reminders = () => {
-    const { reminders } = this.props;
-    return (
-      <ul>
-        {reminders.map((reminder) => {
-          return (
-            <li key={reminder.id} className="group-item">
-              <div>{reminder.text}</div>
-              <div>{reminder.date}</div>
-              <div
-                className="remove btn btn-danger"
-                onClick={() => this.props.remove_Reminder(reminder.id)}
-              >
-                X
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    );
-  };
-
+export default class App extends Component {
   render() {
-    console.log("The props", this.props);
-    return (
-      <div className="App">
-        <div className="app-title">
-          <h2>What Should You Do ?</h2>
-        </div>
-        <input
-          className="form-control"
-          type="text"
-          placeholder="Enter What U Think"
-          onChange={(e) => this.setState({ text: e.target.value })}
-        />
-        <input
-          className="form-control"
-          type="datetime-local"
-          onChange={(e) => this.setState({ date: e.target.value })}
-        />
-        <button
-          onClick={() =>
-            this.props.add_Reminder(this.state.text, this.state.date)
-          }
-          className="btn btn-primary btn-block"
-        >
-          ADD Reminder
-        </button>
-        <div>{this.render_Reminders()}</div>
-        <button className="btn btn-danger btn-block">Clear Reminder</button>
-      </div>
-    );
+    return <h1>Hello world</h1>;
   }
 }
-function mapStateToProps(state) {
-  return {
-    reminders: state,
-  };
-}
-
-/*function mapDispatchToProps(dispatch) {
-  return {
-    Add_Reminder: () => dispatch(add_Reminder()),
-  };
-}*/
-export default connect(mapStateToProps, { add_Reminder, remove_Reminder })(App);
