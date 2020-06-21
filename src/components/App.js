d0ba1ab16f219cbb8c8b9be877464ef392f0c9ef
OmarHassin <boky555555@gmail.com>
2020-06-21 16:18:33 +0200

    Add TodoItems

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import { TodoItems } from "./Todoitems";
 import "../style/App.css";
-
 export default class App extends Component {
+  state = {
+    items: [
+      { id: 1, name: "omar", age: 21 },
+      { id: 2, name: "omar", age: 22 },
+      { id: 3, name: "omar", age: 23 },
+    ],
+  };
+
   render() {
-    return <h1>Hello world</h1>;
+    return (
+      <div className="App">
+        App
+        <TodoItems />
+      </div>
+    );
   }
 }
