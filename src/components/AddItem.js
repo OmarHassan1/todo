3412d9c5182c05670d4dc5aa7d0840eb029a83d1
OmarHassin <boky555555@gmail.com>
2020-06-21 16:22:36 +0200

    Add Additem

---
+++ src/components/AddItem.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export class Additem extends Component {
+  render() {
+    return <div className="Additem">Additem</div>;
+  }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { TodoItems } from "./Todoitems";
+import { Additem } from "./AddItem";
 import "../style/App.css";
 export default class App extends Component {
   state = {
@@ -15,6 +16,7 @@ export default class App extends Component {
       <div className="App">
         App
         <TodoItems />
+        <Additem />
       </div>
     );
   }
